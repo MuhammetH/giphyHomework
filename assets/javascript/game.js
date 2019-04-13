ca0725183e64d16767d531d0b8ff74826d13b4fe
MuhammetD <doktor.oglu333@gmail.com>
2019-04-13 14:41:40 -0500

    can't figure out the problem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,26 @@
-function makeImage(obj) {
-  return `
-    <div>
-      <p>Rating: ${obj.rating}</p>
-      <img src = "${obj.images.fixed_height.url}" />
-    </div>
-    `;
-}
-
 var gifButtons = ["supra", "evil", "eminem", "iron man"];
 
 function makeButton(str) {
   return `<button>${str}</button>`;
-}
-
+};
 
 function renderButtons() {
   $("#buttons").html(gifButtons.map(makeButton));
-}
+};
 
+function makeImage(obj) {
+  return `
+      <div>
+        <p>Rating: ${obj.rating}</p>
+        <img src = "${obj.images.fixed_height.url}" />
+      </div>
+      `;
+};
 
 $("#add-gifs").on("click", function(event) {
   event.preventDefault();
-  var gifbutton = $("#input-gifs").val();
+  var gifbutton = $(this).attr("#input-gifs");
+
   gifButtons.push(gifbutton);
   renderButtons();
 
@@ -30,12 +29,14 @@ $("#add-gifs").on("click", function(event) {
     gifbutton +
     "&api_key=tjraax4CDXA071WNI4qfDUcwxR5cZmvm";
 
-  $.ajax({
+  $.ajaX({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+
     console.log(response);
     $("#gif-view").prepend(response.data.map(makeImage));
+    
   });
 });
 
